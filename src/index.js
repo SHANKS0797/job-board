f952b933ab2ccd6d712d3daa8139df5c506205be
SHANKS0797 <shashankjadhav7997@gmail.com>
2022-09-26 20:44:27 +0530

    Updated route

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/shanks0797.github.io/job-board'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
