49f03d4e315cc135068a933d793c93935e4f9231
SHANKS0797 <shashankjadhav7997@gmail.com>
2022-09-26 17:22:12 +0530

    Updates

---
+++ precache-manifest.2d3ed5a0d5bd872ee178bbf0f5adf499.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "52570d3fb89a653ed510",
+    "url": "/job-board/static/css/main.34897a13.chunk.css"
+  },
+  {
+    "revision": "52570d3fb89a653ed510",
+    "url": "/job-board/static/js/main.52570d3f.chunk.js"
+  },
+  {
+    "revision": "3f8f8df8f3b7f84768bc",
+    "url": "/job-board/static/js/1.3f8f8df8.chunk.js"
+  },
+  {
+    "revision": "45a3381eac53aebade75",
+    "url": "/job-board/static/js/2.45a3381e.chunk.js"
+  },
+  {
+    "revision": "1a13078ef448e9726e05",
+    "url": "/job-board/static/js/runtime~main.1a13078e.js"
+  },
+  {
+    "revision": "1d81d9f08f26ec0534f2128bb8881b08",
+    "url": "/job-board/index.html"
+  }
+];
